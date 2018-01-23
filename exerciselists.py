d632cc8935c7719f86bdbdb36ef9e1bff9704e3b
Coach Fred <alfred.farrington@lusf.org>
2018-01-23 09:17:13 -0500

    basics on creating a list adding, moving, popping and looping. Then printing

---
+++ exerciselists.py
@@ -0,0 +1,22 @@
+# create a list
+exercises = ["bench press", "squat", "deadlift"]
+
+# add to the list
+exercises.append("power clean")
+
+# how to remove from the list
+# exercises.pop(0)  # 0 is the first one in the list
+
+# Let's build a loop
+#for x in range(0,len(exercises)):
+#    exercises[x]
+#    print
+
+# creating a sorted list
+#exercises.sort()
+
+# print an item on the exercise list
+#print(exercises[1]) # this prints out the second item on the list
+#print(exercises)
+
+
