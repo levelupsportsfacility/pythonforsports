4f085699e808a7ef45ef6bbe951dc289fcb2a1b9
Coach Fred <alfred.farrington@lusf.org>
2018-01-23 09:23:11 -0500

    this is a basic function, functions are important.

---
+++ exercisefunction.py
@@ -0,0 +1,6 @@
+def exerciseworld(): # here you are creating a new function
+    print("I love to exercise")
+    return
+# thexese past two lines you are defining it
+
+exerciseworld() # now you are executing it
